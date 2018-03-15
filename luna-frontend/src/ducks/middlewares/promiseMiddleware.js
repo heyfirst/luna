ca4777ddc03cba180df3f381351f8da1606b1782
Kanisorn S <firstziiz.k@gmail.com>
2018-03-15 16:52:26 +0700

    add morgan to package.json

---
+++ luna-frontend/src/ducks/middlewares/promiseMiddleware.js
@@ -0,0 +1,25 @@
+export default () => next => (action) => {
+  if (!action.promise) {
+    return next(action)
+  }
+  next({
+    ...action,
+    type: action.type.PENDING
+  })
+  const actionPromise = new Promise((resolve, reject) =>
+    action.promise
+      .then(response => {
+        return resolve(
+          next({ ...action, type: action.type.RESOLVED, ...response })
+        )
+      }
+      )
+      .catch(error => {
+        return reject(
+          next({ ...action, type: action.type.REJECTED, error })
+        )
+      }
+      )
+  )
+  return actionPromise
+}

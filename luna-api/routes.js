454e5f1a26e70e233e76f060a4115113b2a4b5ab
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-03-24 17:14:15 +0700

    [EDIT]  routes in api routes

--- luna-api/routes.js
+++ luna-api/routes.js
@@ -18,23 +18,3 @@ router.use('/solve', solveRoutes)
 router.use('/topics', topicRoutes)
 
 module.exports = router
-const Router = require('express').Router 
-const router = Router() 
- 
-const helloRoutes = require('./api/hello/routes') 
-const solveRoutes = require('./api/solve/routes') 
-// const tasksRoutes = require('./api/tasks/routes') 
-const topicRoutes = require('./api/topic/routes') 
- 
-router.get('/', async (req, res) => { 
-  res.status(200).send({ 
-    status: `[${process.env.PROJECT_NAME}] API Server is running!` 
-  }) 
-}) 
- 
-router.use('/hello', helloRoutes) 
-router.use('/solve', solveRoutes) 
-// router.use('/tasks', tasksRoutes) 
-router.use('/topics', topicRoutes) 
- 
-module.exports = router 

bbb42798a68942609af079181e4d7e1bdfe36e2e
Kanisorn S <firstziiz.k@gmail.com>
2018-03-29 16:47:39 +0700

    [UPDATE] change utils to libs

--- luna-sandy/libs/jeager-tracing.js
+++ luna-sandy/libs/jeager-tracing.js
@@ -2,21 +2,21 @@ import config from '../config'
 var initJaegerTracer = require('jaeger-client').initTracer
 
 var conf = {
-  'serviceName': config.projectName,
-  'sampler': {
-    'type': 'const',
-    'param': 1
+  serviceName: config.projectName,
+  sampler: {
+    type: 'const',
+    param: 1
   },
-  'reporter': {
-    'logSpans': true
+  reporter: {
+    logSpans: true
   }
 }
 var options = {
-  'logger': {
-    'info': function logInfo (msg) {
+  logger: {
+    info: function logInfo (msg) {
       console.log('INFO ', msg)
     },
-    'error': function logError (msg) {
+    error: function logError (msg) {
       console.log('ERROR', msg)
     }
   }

aeb06e253be7ebf72fc781271061ef9bc8091a72
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-04-03 20:57:37 +0700

    [ADD] topic reducers

--- luna-frontend/src/ducks/reducers/index.js
+++ luna-frontend/src/ducks/reducers/index.js
@@ -1,9 +1,10 @@
 import hello from './hello'
 import count from './count'
 import solve from './solve'
-
+import topic from './topic'
 export default {
   hello,
   count,
-  solve
+  solve,
+  topic
 }

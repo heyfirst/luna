e0b716a1ff12bf76fdf444f75ac5aa34e4222adf
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-04-25 18:20:43 +0700

    [ADD] model topic

--- luna-api/api/topic/model.js
+++ luna-api/api/topic/model.js
@@ -3,17 +3,80 @@ const topic = [{
     topicLevelID: 1,
     taskTopicID: 1,
     title: `Data Type`,
-    description: `This is Topic Data Type`,
+    description: `Lorem Ipsum is not simply random text.`,
     timeStamp: Date()
 },
 {
     topicID: 2,
+    topicLevelID: 2,
+    taskTopicID: 2,
+    title: `String`,
+    description: `Lorem Ipsum is not simply random text.`,
+    timeStamp: Date()
+},
+{
+    topicID: 3,
     topicLevelID: 3,
-    taskTopicID: 1,
+    taskTopicID: 3,
     title: `Array`,
-    description: `This is Topic Array`,
+    description: `Lorem Ipsum is not simply random text.`,
+    timeStamp: Date()
+},
+{
+    topicID: 4,
+    topicLevelID: 1,
+    taskTopicID: 1,
+    title: `Loop`,
+    description: `Lorem Ipsum is not simply random text.`,
+    timeStamp: Date()
+},
+{
+    topicID: 5,
+    topicLevelID: 2,
+    taskTopicID: 2,
+    title: `Condition`,
+    description: `Lorem Ipsum is not simply random text.`,
+    timeStamp: Date()
+},
+{
+    topicID: 6,
+    topicLevelID: 3,
+    taskTopicID: 3,
+    title: `Data Structure`,
+    description: `Lorem Ipsum is not simply random text.`,
+    timeStamp: Date()
+},
+{
+    topicID: 7,
+    topicLevelID: 1,
+    taskTopicID: 1,
+    title: `Algorithm`,
+    description: `Lorem Ipsum is not simply random text.`,
     timeStamp: Date()
 }]
+
+const user = {
+        userID: 1,
+        fullName: `Janjie beauty`,
+        userName: `Janjie beauty`,
+        userScore: [{
+            userScoreID: 1,
+            score: 99,
+            topicID: 1,
+            topicName: "Data Type",
+            Task: [{
+                taskID: 1,
+                taskName: "Plus",
+                timeStamp: Date()
+            },
+            {
+                taskID: 2,
+                taskName: "Minus",
+                timeStamp: Date()
+            }],
+            timeStamp: Date()
+        }]
+    }
 
 module.exports = {
     getAll: () => {
@@ -21,5 +84,8 @@ module.exports = {
     },
     getOne: (id) => {
         return topic.filter(t => t.topicID == id)[0]
-    }
+    },
+    getUserScore: (id) => {
+        return user
+    },
 }

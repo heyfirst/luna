ece41d0b9452e058bef8a0934a2b704ad992fb5b
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-21 16:53:34 +0700

    [ADD] myproject in luna-api-django

--- luna-api-django/entrypoint.sh
+++ luna-api-django/entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 cd /app/
-gunicorn lunaproject.wsgi -b 0.0.0.0:8000
+gunicorn lunaproject.wsgi --workers=3 -b 0.0.0.0:8000

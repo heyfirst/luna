95cbe63525488fbe910db9e566bd685401b45e0c
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-24 21:18:26 +0700

    [FIX] test python runserver

--- luna-api-django/entrypoint.sh
+++ luna-api-django/entrypoint.sh
@@ -3,4 +3,5 @@ cd /app/myproject
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn -w 3 -b 0.0.0.0:8000 myproject.wsgi:application
+# gunicorn -w 3 -b 0.0.0.0:8000 myproject.wsgi:application
+python manage.py runserver

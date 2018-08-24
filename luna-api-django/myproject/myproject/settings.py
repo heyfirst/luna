366c3d29eebe581e5fdf8459a089724e9255c1d4
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-24 23:33:01 +0700

    [EDIT] nginx.conf

--- luna-api-django/myproject/myproject/settings.py
+++ luna-api-django/myproject/myproject/settings.py
@@ -127,4 +127,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "sfiles"), )
 # STATIC_ROOT = "/app/myproject/static"

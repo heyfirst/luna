b196de34bad4000c9825a6d01e4782284745abf6
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-24 20:52:15 +0700

    [ADD] Admin Static file

--- luna-api-django/myproject/myproject/settings.py
+++ luna-api-django/myproject/myproject/settings.py
@@ -41,14 +41,6 @@ INSTALLED_APPS = [
     'topic',
 ]
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',

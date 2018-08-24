9840ce0c62ef2fbf6223b9c559b95e9f24293bf0
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-25 01:51:14 +0700

    [FIX] Check ENV in docker

--- luna-api-django/entrypoint.sh
+++ luna-api-django/entrypoint.sh
@@ -5,6 +5,4 @@ python manage.py migrate
 python manage.py collectstatic --clear --noinput
 python manage.py collectstatic --noinput
 gunicorn -w 3 -b 0.0.0.0:8000 myproject.wsgi:application
-
-rc-update add nginx default
-rc-service nginx start
+/etc/init.d/nginx start

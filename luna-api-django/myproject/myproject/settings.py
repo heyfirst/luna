dfa1393d498759d0c9e34f7c0f5b25d4e1e7d789
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-25 01:35:39 +0700

    [EDIT] Remove SECRET_KEY

--- luna-api-django/myproject/myproject/settings.py
+++ luna-api-django/myproject/myproject/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'pa(pf&n1rhh#co%sy6dkjd!-zka9s^a^yxa5@o^saz47!59@80'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'pa(pf&n1rhh#co%sy6dkjd!-zka9s^a^yxa5@o^saz47!59@80'
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True

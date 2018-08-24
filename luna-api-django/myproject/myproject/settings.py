4201669666366194edf10380ca415bdc20a61f05
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-24 22:16:15 +0700

    [EDIT] STATIC_ROOT

--- luna-api-django/myproject/myproject/settings.py
+++ luna-api-django/myproject/myproject/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pa(pf&n1rhh#co%sy6dkjd!-zka9s^a^yxa5@o^saz47!59@80'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -125,4 +126,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/app/myproject/static"

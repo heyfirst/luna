ba066e17b669fa4b25cb54737ceb1fb9e3b51ffc
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-24 21:34:34 +0700

    [EDIT] entrypoint.sh to use gunicorn

--- luna-api-django/entrypoint.sh
+++ luna-api-django/entrypoint.sh
@@ -3,5 +3,4 @@ cd /app/myproject
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-# gunicorn -w 3 -b 0.0.0.0:8000 myproject.wsgi:application
-python manage.py runserver
+gunicorn -w 3 -b 0.0.0.0:8000 myproject.wsgi:application

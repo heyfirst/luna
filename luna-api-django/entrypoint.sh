684c13fadb27c83c7d306659c8896a02c6c29971
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-26 02:26:13 +0700

    [ADD] start nginx command and test nginx

--- luna-api-django/entrypoint.sh
+++ luna-api-django/entrypoint.sh
@@ -3,6 +3,5 @@ cd /app/myproject
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --clear --noinput
-gunicorn -w 3 -b 0.0.0.0:8000 myproject.wsgi:application
-rc-update add nginx default
+gunicorn -w 3 -b :8000 myproject.wsgi:application
 rc-service nginx start

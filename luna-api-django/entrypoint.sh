2749f268944cf5a04b08c8b264e2c7cbf7a362eb
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-26 18:02:55 +0700

    [FIX] test not use gunicorn

--- luna-api-django/entrypoint.sh
+++ luna-api-django/entrypoint.sh
@@ -3,6 +3,5 @@ cd /app/myproject
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --clear --noinput
-gunicorn -w 3 -b :8000 myproject.wsgi:application
-# rc-service nginx start
-# nginx -t
+# gunicorn -w 3 -b :8000 myproject.wsgi:application
+python manage.py runserver 0.0.0.0:8000 --insecure
--- luna-api-django/myproject/myproject/settings.py
+++ luna-api-django/myproject/myproject/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
 
 # MEDIA_URL='/media/'
 # MEDIA_ROOT=os.path.join(BASE_DIR, 'media/')
 
-STATIC_ROOT = '/var/www/luna/static'
+# STATIC_ROOT = '/var/www/luna/static'

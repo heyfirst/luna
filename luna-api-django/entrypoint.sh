3c8bf72a58b30656039b8c8baeb78340d783a258
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-26 17:05:28 +0700

    [EDIT] comment nginx on alpine & change alias to root

--- luna-api-django/entrypoint.sh
+++ luna-api-django/entrypoint.sh
@@ -4,5 +4,5 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --clear --noinput
 gunicorn -w 3 -b :8000 myproject.wsgi:application
-rc-service nginx start
-nginx -t
+# rc-service nginx start
+# nginx -t

ad7695aac305a53c036fcaf71423fbcb3183c372
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-08-27 23:14:59 +0700

    [EDIT] Path urls

--- luna-api-django/myproject/myproject/urls.py
+++ luna-api-django/myproject/myproject/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path(r'api/', include('topic.urls')),
-    path(r'admin/', admin.site.urls),
+    path('api/', include('topic.urls')),
+    path('admin/', admin.site.urls),
     
 ]

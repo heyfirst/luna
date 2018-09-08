84ba8fe327ab03b9761e1306c71ae5e6881839e4
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-09-08 22:04:07 +0700

    [ADD] views in tasks

--- luna-gateway/luna_gateway/tasks/views.py
+++ luna-gateway/luna_gateway/tasks/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Q
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskList(APIView):
+    def get(self, request, format=None):
+        tasks = Task.objects.all().order_by('id')
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+class TaskDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)

82960e24333dd33834785561cf73465af84583ac
Unknown <veeraporn.m@mail.kmutt.ac.th>
2018-09-08 22:03:13 +0700

    [EDIT] views and urls in topics

--- luna-gateway/luna_gateway/topics/urls.py
+++ luna-gateway/luna_gateway/topics/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     url(r'^topics/$', views.TopicList.as_view()),
+    url(r'^topics/(?P<pk>[0-9]+)/$', views.TopicDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- luna-gateway/luna_gateway/topics/views.py
+++ luna-gateway/luna_gateway/topics/views.py
@@ -5,9 +5,20 @@ from rest_framework.response import Response
 from .models import Topic
 from .serializers import TopicSerializer
 
-
 class TopicList(APIView):
     def get(self, request, format=None):
         topics = Topic.objects.all()
         serializer = TopicSerializer(topics, many=True)
+        return Response(serializer.data)
+
+class TopicDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Topic.objects.get(pk=pk)
+        except Topic.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        topic = self.get_object(pk)
+        serializer = TopicSerializer(topic)
         return Response(serializer.data)

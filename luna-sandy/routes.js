a0a82f3d6f784170a32b4140f6010a0a8c537f6a
Kanisorn S <firstziiz.k@gmail.com>
2018-09-28 04:01:16 +0700

    Add new machanism

--- luna-sandy/routes.js
+++ luna-sandy/routes.js
@@ -1,74 +1,118 @@
-const util = require('util')
-const exec = util.promisify(require('child_process').exec)
-const fs = require('fs')
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const fs = require("fs");
 
-const Router = require('express').Router
-const router = Router()
+const Router = require("express").Router;
+const router = Router();
 
-const createJavaFile = (filepath, data) => {
+const createJavaFile = (filepath, data, testcases) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(filepath, data, function (err) {
-      if (err) reject(err)
-      else resolve(data)
-    })
-  })
-}
+    const defaultClass = `
+      class Main {
+        static ${data}
 
-const compileJava = async (filepath) => {
-  const compileCmd = `javac ${filepath}`
-  const { stdout, stderr } = await exec(compileCmd)
+        public static void main(String[] args){
+          ${testcases.map(t => `System.out.println(${t.input});`).join("\n")}
+        }
+
+      }
+    `;
+
+    fs.writeFile(filepath, defaultClass, function(err) {
+      if (err) reject(err);
+      else resolve(defaultClass);
+    });
+  });
+};
+
+const removeUserFolder = async userID => {
+  const compileCmd = `cd submissions && rm -rf user-${userID}`;
+  const { stdout, stderr } = await exec(compileCmd);
   return {
     stdout,
     stderr
-  }
-}
+  };
+};
 
-const runningTest = async (input) => {
-  const runCmd = `cd judges && java Sandbox ${input}`
-  const { stdout, stderr } = await exec(runCmd)
+const createUserFolder = async userID => {
+  const compileCmd = `cd submissions && mkdir user-${userID}`;
+  const { stdout, stderr } = await exec(compileCmd);
   return {
     stdout,
     stderr
-  }
-}
+  };
+};
 
-router.get('/', async (req, res) => {
+const compileJava = async filepath => {
+  const compileCmd = `javac ${filepath}`;
+  const { stdout, stderr } = await exec(compileCmd);
+  return {
+    stdout,
+    stderr
+  };
+};
+
+const runningTest = async classFilepath => {
+  const runCmd = `cd ${classFilepath} && java Main`;
+  const { stdout, stderr } = await exec(runCmd);
+  return {
+    stdout,
+    stderr
+  };
+};
+
+router.get("/", async (req, res) => {
   res.status(200).send({
     status: `[${process.env.PROJECT_NAME}] API Server is running!`
-  })
-})
+  });
+});
 
-router.post('/run', async (req, res, next) => {
+router.post("/submission", async (req, res, next) => {
+  const user = req.body.user;
+  const testcases = req.body.testcases;
+  const code = req.body.code;
+
   try {
-    const testcase = req.body.testcase
-    const code = req.body.code
+    await removeUserFolder(user.id);
+    // Creat Folder
+    await createUserFolder(user.id);
+    const filepath = `./submissions/user-${user.id}/submission-${user.id}.java`;
+    const classFilepath = `./submissions/user-${user.id}/`;
 
-    const filepath = `./judges/test.java`
-    await createJavaFile(filepath, code)
+    // Create File Java
+    await createJavaFile(filepath, code, testcases, user);
 
-    await compileJava(filepath)
+    // Compile File Java to Main.class
+    await compileJava(filepath);
+    const output = await runningTest(classFilepath);
 
-    const result = testcase.map(async (tc, index) => {
-      const result = await runningTest(tc.input)
-      return {
-        testcase_id: tc.testcase_id,
-        output: result.stdout,
-        status: result.stdout === tc.expected_output
-      }
-    })
+    // Compare Input and Output
+    const result = await output.stdout
+      .trim()
+      .split("\n")
+      .map((result, index) => {
+        return {
+          status: result === testcases[index].output
+        };
+      });
 
-    Promise.all(result).then((resp) => {
-      console.log(resp)
+    Promise.all(result).then(async resp => {
+      // Remove all files of user
+      await removeUserFolder(user.id);
       res.status(200).send({
-        status: 'PASS',
+        status: "PASS",
         result: resp
-      })
-    })
+      });
+      return;
+    });
   } catch (err) {
+    // Remove all files of user
+    await removeUserFolder(user.id);
     res.status(500).send({
       err
-    })
+    });
+    return;
   }
-})
+});
 
-module.exports = router
+module.exports = router;

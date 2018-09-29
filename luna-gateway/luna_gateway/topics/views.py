94ef639a509af5e8daa3911c9f493897d28bb361
Kanisorn S <firstziiz.k@gmail.com>
2018-09-30 03:18:08 +0700

    All total tasks and completed tasks

--- luna-gateway/luna_gateway/topics/views.py
+++ luna-gateway/luna_gateway/topics/views.py
@@ -2,6 +2,7 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
 from answers.models import Answer
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
@@ -10,13 +11,23 @@ from .serializers import TopicSerializer, LevelSerializer, TopicLevelReadSeriali
 
 
 class TopicViewSet(viewsets.ModelViewSet):
+    queryset = Topic.objects.all()
     serializer_class = TopicSerializer
     filter_fields = ('__all__')
     ordering_fields = '__all__'
     ordering = ('pk', )
 
-    def get_queryset(self):
-        return Topic.objects.all()
+    def list(self, request, *args, **kwargs):
+        user = request.user
+        queryset = self.filter_queryset(self.get_queryset())
+
+        topics_data = self.get_serializer(queryset, many=True).data
+        topics = [
+            self._get_topic_detail_by_user(topic, user)
+            for topic in topics_data
+        ]
+
+        return Response(topics)
 
     @action(detail=True)
     def tasks(self, request, pk=None):
@@ -31,7 +42,10 @@ class TopicViewSet(viewsets.ModelViewSet):
             'task', flat=True
         )
 
-        tasks = Task.objects.filter(main_topic__topic__pk=pk)
+        tasks = Task.objects.filter(
+            main_topic__topic__pk=pk,
+            order__isnull=False,
+        )
 
         tasks_data = TaskSerializer(
             tasks,
@@ -53,6 +67,22 @@ class TopicViewSet(viewsets.ModelViewSet):
         if task['id'] in answered_task_pks:
             task['answered'] = True
         return task
+
+    def _get_topic_detail_by_user(self, topic, user):
+        completed_task = Answer.objects.filter(
+            owned_by=user,
+            task__main_topic__pk=topic['pk'],
+        ).count()
+        total_tasks = Task.objects.filter(
+            main_topic__topic__pk=topic['pk'],
+            order__isnull=False,
+        ).count()
+
+        return {
+            **topic,
+            "total_tasks": total_tasks,
+            "completed_tasks": completed_task,
+        }
 
 
 class LevelViewSet(viewsets.ModelViewSet):

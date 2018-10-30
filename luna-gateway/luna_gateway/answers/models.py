880568b587680216aeb4ca2c51fa0a48f9ef684b
Kanisorn S <firstziiz.k@gmail.com>
2018-10-30 16:55:20 +0700

    [ADD] add SolutionVote model

--- luna-gateway/luna_gateway/answers/models.py
+++ luna-gateway/luna_gateway/answers/models.py
@@ -10,6 +10,31 @@ class Answer(Timestamp):
     task = models.ForeignKey('tasks.Task', on_delete=models.CASCADE)
     source_code = models.TextField(default='')
     duration = models.DurationField(default=timedelta(seconds=0))
+
+    result = models.BooleanField(default=False)
+
+    owned_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+
+class SolutionVote(Timestamp):
+    answer = models.ForeignKey('answers.Answer', on_delete=models.CASCADE)
+
+    VOTE_TYPES = (
+        ('best_practice', 'ยอดเยี่ยม'),
+        ('clever', 'ฉลาดใช้'),
+    )
+
+    vote_type = models.CharField(
+        null=False,
+        blank=False,
+        choices=VOTE_TYPES,
+        default='',
+        max_length=20,
+    )
+
     owned_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
